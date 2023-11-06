052e593de664460438923d7b4cef62c09457f8c5
Hades <wzawzpopo@qq.com>
2023-11-06 16:45:48 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LunarSwift",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "LunarSwiftTests",
-            dependencies: ["LunarSwift"]),
+            dependencies: ["LunarSwift"],
+            resources: [.process("Resources")]),
     ]
 )
